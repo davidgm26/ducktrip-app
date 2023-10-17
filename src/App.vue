5e17cfb3e78e82ecacb8c1d4ee63bf04e9942f5e
David Garcia <davidgama260402@gmail.com>
2023-10-17 12:23:52 +0200

    Navigation bar desing finish, no routing

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
-<script setup>
+<script>
 import SearchFlight from './components/SearchFlight.vue';
+import NavigationTopBar from './components/NavigationTopBar.vue';
+
+export default {
+  name: 'App',
+  components: {
+    SearchFlight,
+    NavigationTopBar,
+  }
+}
+
 </script>
 
 <template>
   <header>
+    <NavigationTopBar />
   </header>
   <main>
     <SearchFlight />
@@ -12,42 +23,4 @@ import SearchFlight from './components/SearchFlight.vue';
   <RouterView />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<style scoped></style>
