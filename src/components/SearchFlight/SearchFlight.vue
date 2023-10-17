d065cdac15fb9bd736d8a1ed3a3873ad1d51bdc5
Elena Expósito <elexla97@gmail.com>
2023-10-17 14:30:19 +0200

    Add dropdown functionality

--- src/components/SearchFlight/SearchFlight.vue
+++ src/components/SearchFlight/SearchFlight.vue
@@ -25,8 +25,13 @@
                     <input class="nav-description" type="date">
                 </div>
                 <div class="option-wrapper">
-                    <label class="nav-info">Viajeros</label>
-                    <input class="nav-description" type="string" placeholder="Número de adultos">
+
+                    <label class="nav-info" @click="toggleDropdown">Viajeros</label>
+                    <p class="nav-description" type="string" @click="toggleDropdown">
+                        {{ adultCount }} adultos
+                    </p>
+                    <DropDown :isVisible=isDropdownOpen :adultCount=adultCount :incrementAdults=incrementAdults
+                        :decrementAdults=decrementAdults :onReadyClick=handleReadyClick />
                 </div>
             </div>
             <button class="search">Buscar</button>
@@ -34,16 +39,49 @@
     </div>
 </template>
 
+<script>
+import DropDown from "./components/DropDown/DropDown.vue"
+export default {
+    components: {
+        DropDown
+    },
+    data() {
+        return {
+            isDropdownOpen: false,
+            dataFromDropDown: "",
+            adultCount: 1
+        };
+    },
+    methods: {
+        toggleDropdown() {
+            this.isDropdownOpen = !this.isDropdownOpen;
+        },
+        incrementAdults() {
+            this.adultCount++;
+        },
+        decrementAdults() {
+            this.adultCount = Math.max(this.adultCount - 1, 1);
+        },
+        receiveDataFromDropDown(data) {
+            this.dataFromDropDown = data;
+        },
+        handleReadyClick() {
+            this.isDropdownOpen = false;
+        },
+    }
+};
+</script>
+
 <style>
 .nav {
-    background-color: rgb(27, 85, 176);
+    background-color: #1b55b0;
     width: 100%;
     padding: 1rem 2rem;
 }
 
 .nav-tittle {
     color: #FFFFFF;
-    text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    text-shadow: 0px 4px 4px #00000040;
     font-size: 40px;
     font-style: normal;
     font-weight: 400;
@@ -60,7 +98,7 @@
 .nav-button {
     border-radius: 10px;
     border: none;
-    background: rgba(0, 33, 72, 0.5);
+    background: #00214880;
     color: #FFFFFF;
     cursor: pointer;
     font-size: 1rem;
@@ -75,27 +113,36 @@
 
 .nav-options {
     display: flex;
-    border-radius: 0.5rem;
     gap: 0.25rem;
-    overflow: hidden;
 }
 
+
 .option-wrapper {
-    background-color: rgba(255, 255, 255, 0.83);
+    background-color: #ffffffd4;
     display: flex;
     flex-direction: column;
     padding: 0.5rem 1rem;
     gap: 0.25rem;
+    position: relative;
+}
+
+.option-wrapper:first-child {
+    border-radius: 0.5rem 0 0 0.5rem;
+}
+
+.option-wrapper:last-child {
+    border-radius: 0 0.5rem 0.5rem 0;
 }
 
 .nav-info {
     color: #393838;
-    text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    text-shadow: 0px 4px 4px #00000040;
 }
 
 .nav-description {
     background-color: transparent;
     border: none;
+    color: #393838;
 }
 
 .search {
@@ -107,4 +154,3 @@
     padding: 0 1rem;
 }
 </style>
-
