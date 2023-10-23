45ceb920569df0acf51cee91d5206ea83d1a9cd9
Elena Expósito <elexla97@gmail.com>
2023-10-23 15:05:47 +0200

    TipsPage.vue template, script and style. Add ProgressBar.vue

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -3,14 +3,16 @@
     <div>
         <SearchFlight />
     </div>
+    <ProgressBar />
 </template>
 <script>
 import NavigationTopBar from '../components/NavigationTopBar.vue';
 import SearchFlight from '../components/SearchFlight/SearchFlight.vue';
+import ProgressBar from '../components/ProgressBar.vue';
 
 export default {
     name: 'HomePage',
-    components: { NavigationTopBar, SearchFlight }
+    components: { NavigationTopBar, ProgressBar, SearchFlight }
 }
 </script>
 <style scoped></style>
--- src/views/TipsPage.vue
+++ src/views/TipsPage.vue
@@ -1,5 +1,6 @@
 <template>
     <NavigationTopBar />
+    <ProgressBar />
     <main class="main">
         <section class="tips">
             <h1 class="tips-title">Tips interesantes para viajar</h1>
@@ -54,7 +55,7 @@
             </div>
 
             <h2 class="tip-title" id="title1">1. Investiga y planifica</h2>
-            <img src="" alt="" class="tip-image">
+            <img src="@/assets/images/tips-image1.jpg" alt="Investigar y planiificar el viaje" class="tip-image">
             <p class="paragraph">Investiga sobre tu <b>destino</b>, su <b>cultura</b>, <b>costumbres</b> y
                 <b>atracciones</b> antes de
                 viajar. Planificar con antelación te ayudará a sacar el máximo provecho de tu viaje.
@@ -63,13 +64,14 @@
                 necesaria para disfrutar al máximo de tu aventura sin preocupaciones innecesarias.
             </p>
             <h2 class="tip-title" id="title2">2. Packing inteligente</h2>
-            <img src="" alt="" class="tip-image">
+            <img src="@/assets/images/tips-image2.jpg" alt="Hacer la maleta de forma inteligente" class="tip-image">
             <p class="paragraph">Haz tu maleta de forma inteligente, lleva <b>solo lo necesario</b> y elige <b>ropa
                     versátil</b>.
                 No
                 olvides
                 elementos esenciales como adaptadores de corriente y artículos de cuidado personal.</p>
-            <p class="paragraph">Para ello, te recomendamos hacer una lista como la siguiente que te ayude a evitar olvidos
+            <p class="paragraph">Para ello, te recomendamos hacer una <b>lista</b> como la siguiente que te ayude a evitar
+                olvidos
                 de último minuto:</p>
 
             <div class="list-container">
@@ -85,36 +87,60 @@
             </div>
 
             <h2 class="tip-title" id="title3">3. Documentos importantes</h2>
-            <img src="" alt="" class="tip-image">
-            <p class="paragraph">Asegúrate de llevar contigo todos los documentos necesarios, como pasaportes, visas,
-                boletos y copias de seguridad electrónicas de los mismos.</p>
+            <img src="@/assets/images/tips-image3.jpg" alt="Llevar los documentos necesarios" class="tip-image">
+            <p class="paragraph">Asegúrate de llevar contigo todos los documentos necesarios, como <b>pasaporte</b>,
+                <b>DNI</b>, <b>visas</b>,
+                <b>tickets</b> y <b>copias de seguridad electrónicas</b> de los mismos.
+            </p>
             <h2 class="tip-title" id="title4">4. Seguro de viaje</h2>
-            <img src="" alt="" class="tip-image">
-            <p class="paragraph">Considera la posibilidad de obtener un seguro de viaje que cubra emergencias médicas,
-                cancelaciones y pérdida de equipaje. Proporciona tranquilidad en caso de imprevistos.</p>
+            <img src="@/assets/images/tips-image4.jpg" alt="Obtener un seguro de viaje" class="tip-image">
+            <p class="paragraph">Considera la posibilidad de obtener un seguro de viaje que cubra <b>emergencias
+                    médicas</b>,
+                <b>cancelaciones</b> y <b>pérdida de equipaje</b>. Proporciona tranquilidad en caso de imprevistos.
+            </p>
             <h2 class="tip-title" id="title5">5. Presupuesto adecuado</h2>
-            <img src="" alt="" class="tip-image">
-            <p class="paragraph">Establece un presupuesto para tu viaje y trata de ceñirte a él. Lleva un registro de tus
-                gastos para evitar sorpresas desagradables.</p>
+            <img src="@/assets/images/tips-image5.jpg" alt="Establecer un presupuesto" class="tip-image">
+            <p class="paragraph">Establece un <b>presupuesto</b> para tu viaje y trata de ceñirte a él. Lleva un
+                <b>registro</b> de
+                tus
+                gastos para evitar sorpresas desagradables.
+            </p>
+            <p class="paragraph">Te aconsejamos <b>tener en cuenta</b> lo siguiente:
+            <ul class="budget-list">
+                <li class="budget-item">• Establecer un presupuesto inicial</li>
+                <li class="budget-item">• Seguimiento de gastos</li>
+                <li class="budget-item">• Planificación de actividades</li>
+                <li class="budget-item">• Considera gastos ocultos</li>
+                <li class="budget-item">• Reserva con antelación</li>
+                <li class="budget-item">• Efectivo y tarjetas</li>
+                <li class="budget-item">• Fondo de contingencia en caso de emergencias</li>
+                <li class="budget-item">• Conversión de divisas</li>
+            </ul>>
+            </p>
             <h2 class="tip-title" id="title6">6. Aprende algunas frases locales</h2>
-            <img src="" alt="" class="tip-image">
-            <p class="paragraph">Aprender algunas frases básicas en el idioma local puede facilitar la comunicación y
+            <img src="@/assets/images/tips-image6.jpg" alt="Aprender frases básicas en el idioma local" class="tip-image">
+            <p class="paragraph">Aprender algunas <b>frases básicas</b> en el <b>idioma local</b> puede facilitar la
+                comunicación y
                 mostrar respeto por la cultura local.</p>
             <h2 class="tip-title" id="title7">7. Viaja ligero</h2>
-            <img src="" alt="" class="tip-image">
-            <p class="paragraph">Lleva una mochila pequeña o maleta de mano para evitar cargas innecesarias y agilizar el
+            <img src="@/assets/images/tips-image7.jpg" alt="Llevar una mochila o maleta pequeña" class="tip-image">
+            <p class="paragraph">Lleva una <b>mochila pequeña</b> o <b>maleta de mano</b> para evitar cargas innecesarias y
+                agilizar el
                 proceso de registro y desplazamiento.</p>
             <h2 class="tip-title" id="title8">8. Mantén la mente abierta</h2>
-            <img src="" alt="" class="tip-image">
-            <p class="paragraph">Sé flexible y dispuesto a probar cosas nuevas. Los viajes ofrecen oportunidades únicas para
+            <img src="@/assets/images/tips-image8.jpg" alt="Mantener la mente abierta" class="tip-image">
+            <p class="paragraph">Sé <b>flexible</b> y dispuesto a <b>probar cosas nuevas</b>. Los viajes ofrecen
+                oportunidades únicas para
                 experimentar culturas diferentes.</p>
             <h2 class="tip-title" id="title9">9. Interactúa con locales</h2>
-            <img src="" alt="" class="tip-image">
-            <p class="paragraph">Conoce a la gente local y haz amigos. Ellos pueden proporcionarte consejos valiosos y
-                enriquecer tu experiencia.</p>
+            <img src="@/assets/images/tips-image9.jpg" alt="Interactuar con la gente local" class="tip-image">
+            <p class="paragraph">Conoce a la <b>gente local</b> y haz amigos. Ellos pueden proporcionarte <b>consejos
+                    valiosos y
+                    enriquecer tu experiencia</b>.</p>
             <h2 class="tip-title" id="title10">10. Haz copias de seguridad de tus fotos y documentos</h2>
-            <img src="" alt="" class="tip-image">
-            <p class="paragraph">Utiliza servicios de almacenamiento en la nube para respaldar tus fotos y documentos
+            <img src="@/assets/images/tips-image10.jpg" alt="Utilizar servicios de almacenamiento en la nube"
+                class="tip-image">
+            <p class="paragraph">Utiliza <b>servicios de almacenamiento en la nube</b> para respaldar tus fotos y documentos
                 importantes, como pasaportes y reservas de hotel, en caso de pérdida o robo.</p>
         </section>
     </main>
@@ -122,16 +148,20 @@
 
 <script>
 import NavigationTopBar from '../components/NavigationTopBar.vue';
+import ProgressBar from '../components/ProgressBar.vue';
 
 export default {
     name: 'TipsPage',
-    components: { NavigationTopBar },
+    components: { NavigationTopBar, ProgressBar },
     data() {
         return {
             listaMaleta: [
-                { name: "prueba1", checked: false },
-                { name: "prueba2", checked: false },
-                { name: "prueba3", checked: false },
+                { name: "Ropa, ropa interior y calzado", checked: false },
+                { name: "Bolsa de aseo", checked: false },
+                { name: "Documento de identificación", checked: false },
+                { name: "Dinero en efectivo y tarjetas", checked: false },
+                { name: "Medicamentos y paraguas", checked: false },
+                { name: "Cargador teléfono y adaptador", checked: false },
             ],
         };
     },
@@ -182,11 +212,18 @@ export default {
     line-height: normal;
 }
 
-.header-image,
+.header-image {
+    border-radius: 1.5rem;
+    box-shadow: 5px 5px 10px #3E86F5;
+    margin: 1rem 0;
+    width: 100%;
+}
+
 .tip-image {
     border-radius: 1.5rem;
     box-shadow: 5px 5px 10px #3E86F5;
     margin: 1rem 0;
+    max-width: 600px;
     width: 100%;
 }
 
@@ -241,8 +278,8 @@ export default {
     flex: 1 1 100%;
     display: table;
     margin: 0 auto 40px;
-    max-width: 400px;
-    padding: 2rem;
+    max-width: 700px;
+    padding: 4rem 2rem 2rem 2rem;
     position: relative;
     width: 100%;
 }
@@ -251,16 +288,40 @@ export default {
     display: flex;
     justify-content: center;
     color: #ffffff;
-
+    text-shadow: 0px 4px 4px #00000040;
+    font-size: 1rem;
+    font-weight: 600;
 }
 
 .list-item {
     display: flex;
     justify-content: center;
+    color: #ffffff;
+    text-shadow: 0px 4px 4px #00000040;
+    font-size: 0.9rem;
     gap: 0.5rem;
 }
 
 .checked {
     text-decoration: line-through;
+}
+
+.budget-list {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+}
+
+.budget-item {
+    display: flex;
+    justify-content: center;
+    background-color: #3E86F5;
+    color: #ffffff;
+    text-shadow: 0px 4px 4px #00000040;
+    font-size: 1.1rem;
+    gap: 0.5rem;
+    border: 1px solid #3E86F5;
+    border-radius: 15px;
+    padding: 0.5rem;
 }
 </style>
