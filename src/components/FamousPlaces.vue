5556088a8422f1c96c4b589a7e6832a70ca1c702
David Garcia <davidgama260402@gmail.com>
2023-10-23 21:51:11 +0200

    Famous places finished

--- src/components/FamousPlaces.vue
+++ src/components/FamousPlaces.vue
@@ -1,7 +1,8 @@
 <template>
+    <h1 class="most-famous-tittle">Los destinos más seleccionados por los pasajeros</h1>
     <div class="card-container">
         <div class="card" v-for="(place, index) in places" :key="index" :style="{ backgroundImage: `url(${place.image})` }">
-            <p>{{ place.name }}</p>
+            <p class="place-info">{{ place.name }} {{ place.score }} ⭐</p>
         </div>
     </div>
 </template>
@@ -14,17 +15,24 @@ export default {
     data() {
         return {
             places: [],
-
         }
     },
     methods: {
         async getSitiosFamosos() {
             this.places = await getMostVisitedPlaces();
             console.log('adios', this.places);
+            /*Guardar fecha de caducidad (today + X) */
+            localStorage.setItem('places', JSON.stringify(this.places))
         }
     },
     async mounted() {
-        await this.getSitiosFamosos()
+        const placesString = localStorage.getItem('places') || '[]';
+        const placesObject = JSON.parse(placesString);
+        if (placesObject.length === 0) {
+            await this.getSitiosFamosos()
+        } else {
+            this.places = placesObject
+        }
     }
 
 }
@@ -34,32 +42,48 @@ export default {
 .card-container {
     margin: 15px;
     min-height: 380px;
-    display: grid;
+    display: flex;
     gap: 14px;
     flex-direction: row;
+
+
+}
+
+.most-famous-tittle {
+    font-size: 2em;
+    margin: 20px;
+
 }
 
 .card {
-    width: 100%;
-    background-color: blue;
-    display: flex;
+    width: 50%;
+    background-color: #ffffff;
+    display: grid;
     flex-direction: row;
     margin: 3px;
     border-radius: 50px;
     height: 500px;
     background-size: cover;
-    border: 1px red solid;
-
+    background-repeat: no-repeat;
+    grid-template-columns: min-content;
+    background-position: center;
+    box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
 
     & .place-info {
-        width: 100%;
-        height: 20px;
+        width: max-content;
+        height: 50px;
         border-radius: 50px;
         margin: 20px;
+        color: #fff;
+        text-shadow: 1px 1px black;
+        background-color: #51465478;
+        text-align: center;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        font-size: small;
+        padding: 10px;
     }
 
-    .place-info img {
-        width: 5%;
-    }
 }
 </style>
--- src/stores/modules/getMostVisitedPlaces.js
+++ src/stores/modules/getMostVisitedPlaces.js
@@ -1,18 +1,21 @@
 import { getFamousPlaces } from './fetchMostVisitedPlacesIATA'
 import { getCityPhoto } from './getCityImage.js';
 import { suggestLocation } from './getFlights.js';
+import defaultImg from '@/assets/images/defaultImage.png'
 
 export const getMostVisitedPlaces = async () =>{
     const mostVisited = await getFamousPlaces();
     const mostVisitedPlaces = await Promise.all(
         mostVisited.map(async (IATA) => {
+            const score = IATA.analytics.travelers.score;
             const name = await suggestLocation(IATA.destination);
             const city = name[0]?.address?.cityName
             const data = await getCityPhoto(city.toLowerCase());
-            const image = data?.photos[0]?.image?.mobile || 'assets/images/defaultImage.png'
+            const image = data?.photos[0]?.image?.mobile || defaultImg
             return{
                 image: image,
                 name:city,
+                score:score/10,
                 iata:IATA.destination
             }
         })
