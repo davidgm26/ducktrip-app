808dcb5358855a4428c95302ab2d5d86c0139c13
Carlos Concepcion <concep131@gmail.com>
2023-10-24 18:42:15 +0100

    SearchFlight and FlightCard working with pinia store

--- src/components/FlightCard.vue
+++ src/components/FlightCard.vue
@@ -1,211 +1,217 @@
 <template>
-    <div class="actual-info-container">
-        <p>Origen: {{ departureIata }}</p>
-        <p>Destino: {{ arrivalIata }}</p>
-        <p>Fecha: {{ departureDate }}</p>
-        <p>Nº de Adultos: {{ adult }}</p>
+  <div class="actual-info-container">
+    <p>Origen: {{ departureIata }}</p>
+    <p>Destino: {{ arrivalIata }}</p>
+    <p>Fecha: {{ departureDate }}</p>
+    <p>Nº de Adultos: {{ adult }}</p>
+  </div>
+  <div class="error-msg" v-if="flightOffers === 0">
+    <p>NO SE HAN ENCONTRADO VUELOS</p>
+  </div>
+  <div class="container" v-for="Fligth in flightOffers.data" :key="Fligth">
+    <div class="airline-logo">
+      <h1 class="ariline-title">
+        iata de aerolinea: {{ Fligth.validatingAirlineCodes[0] }}
+      </h1>
+      <img
+        :src="`https://daisycon.io/images/airline/?width=900&iata=${Fligth.validatingAirlineCodes[0]}`"
+        alt=""
+        srcset=""
+      />
     </div>
-    <div class="error-msg" v-if="flightOffers === 0">
+    <div class="info">
+      <div class="flight-duration">
+        <h1>
+          {{ formatDuration(Fligth.itineraries[0].duration) }}
+        </h1>
+      </div>
+      <div
+        class="flight-segments"
+        v-for="segment in Fligth.itineraries[0].segments"
+        :key="segment.number"
+      >
+        <div class="departures-arrivals">
+          <img src="../assets/icons/departures.svg" alt="" srcset="" />
+          <div class="hour">
+            {{ formatDateHour(segment.departure.at) }}
+          </div>
+          <div class="airport-name">
+            {{ segment.departure.iataCode }}
+          </div>
+        </div>
+        <div class="departures-arrivals">
+          <img src="../assets/icons/arrivals.svg" alt="" srcset="" />
+          <div class="hour">
+            {{ formatDateHour(segment.arrival.at) }}
+          </div>
+          <div class="airport-name">
+            {{ segment.arrival.iataCode }}
+          </div>
+        </div>
+      </div>
+      <div v-if="Fligth.itineraries[0].segments.length > 1">
+        <p class="non-stop">VUELO CON ESCALA</p>
+      </div>
+      <div class="final-price">
         <p>
-            NO SE HAN ENCONTRADO VUELOS
+          Total:
+          {{ Fligth.price.total }}
+          {{ getSymbolForCurrency(Fligth.price.currency) }}
         </p>
+      </div>
     </div>
-    <div class="container" v-for="(Fligth, index) in flightOffers.data" :key="Fligth">
-        <div class="airline-logo">
-            <h1 class="ariline-title">iata de aerolinea: {{ Fligth.validatingAirlineCodes[0] }}</h1>
-            <img :src="`https://daisycon.io/images/airline/?width=900&iata=${Fligth.validatingAirlineCodes[0]}`" alt=""
-                srcset="">
-        </div>
-        <div class="info">
-            <div class="flight-duration">
-                <h1>
-                    {{ formatDuration(Fligth.itineraries[0].duration) }}
-                </h1>
-            </div>
-            <div class="flight-segments" v-for="segment in Fligth.itineraries[0].segments" :key="segment.number">
-                <div class="departures-arrivals">
-                    <img src="../assets/icons/departures.svg" alt="" srcset="">
-                    <div class="hour">
-                        {{ formatDateHour(segment.departure.at) }}
-                    </div>
-                    <div class="airport-name">
-                        {{ segment.departure.iataCode }}
-                    </div>
-
-                </div>
-                <div class="departures-arrivals">
-                    <img src="../assets/icons/arrivals.svg" alt="" srcset="">
-                    <div class="hour">
-                        {{ formatDateHour(segment.arrival.at) }}
-                    </div>
-                    <div class="airport-name">
-                        {{ segment.arrival.iataCode }}
-                    </div>
-                </div>
-            </div>
-            <div v-if="Fligth.itineraries[0].segments.length > 1">
-                <p class="non-stop">VUELO CON ESCALA</p>
-            </div>
-            <div class="final-price">
-                <p>Total: {{ `${flightOffers.data[index].price.total}
-                                    ${getSymbolForCurrency(flightOffers.data[index].price.currency)}` }}</p>
-            </div>
-        </div>
-    </div>
+  </div>
 </template>
 <script>
-import { flightSearchStore } from '../stores/counter'
-import { mapState } from 'pinia'
+import { flightSearchStore } from "../stores/counter";
+import { mapState } from "pinia";
 
 export default {
-    name: "FlightCard",
-    computed: {
-        ...mapState(flightSearchStore, [
-            'departureIata',
-            'arrivalIata',
-            'departureDate',
-            'adult',
-            'flightOffers'
-        ])
-    },
-    methods: {
-        formatDuration(durationString) {
-            const regex = /PT(\d*H)?(\d*M)?/;
-            const match = durationString.match(regex);
-            let hours = 0;
-            let minutes = 0;
-            if (match) {
-                if (match[1]) {
-                    hours = parseInt(match[1], 10);
-                }
-                if (match[2]) {
-                    minutes = parseInt(match[2], 10);
-                }
-            }
-            if (hours > 0 && minutes > 0) {
-                return `La duración es de ${hours}H y ${minutes}M`;
-            } else if (hours > 0) {
-                return `La duración es de ${hours}H`;
-            } else if (minutes > 0) {
-                return `La duración es de ${minutes}M`;
-            } else {
-                return "Duración desconocida";
-            }
-        },
-        formatDateHour(datehour) {
-            return datehour.replace("T", " ");
-        },
-        getSymbolForCurrency(Code) {
-            const currencyCode = {
-                USD: '$',
-                EUR: '€',
-                JPY: '¥',
-                GBP: '£',
-                CAD: 'CA$',
-                AUD: 'A$',
-            }
-            return currencyCode[Code] || '';
+  name: "FlightCard",
+  computed: {
+    ...mapState(flightSearchStore, [
+      "departureIata",
+      "arrivalIata",
+      "departureDate",
+      "adult",
+      "flightOffers",
+    ]),
+  },
+  methods: {
+    formatDuration(durationString) {
+      const regex = /PT(\d*H)?(\d*M)?/;
+      const match = durationString.match(regex);
+      let hours = 0;
+      let minutes = 0;
+      if (match) {
+        if (match[1]) {
+          hours = parseInt(match[1], 10);
         }
+        if (match[2]) {
+          minutes = parseInt(match[2], 10);
+        }
+      }
+      if (hours > 0 && minutes > 0) {
+        return `La duración es de ${hours}H y ${minutes}M`;
+      } else if (hours > 0) {
+        return `La duración es de ${hours}H`;
+      } else if (minutes > 0) {
+        return `La duración es de ${minutes}M`;
+      } else {
+        return "Duración desconocida";
+      }
     },
-
-
-}
+    formatDateHour(datehour) {
+      return datehour.replace("T", " ");
+    },
+    getSymbolForCurrency(Code) {
+      const currencyCode = {
+        USD: "$",
+        EUR: "€",
+        JPY: "¥",
+        GBP: "£",
+        CAD: "CA$",
+        AUD: "A$",
+      };
+      return currencyCode[Code] || "";
+    },
+  },
+};
 </script>
 <style scoped>
 .container {
-    width: 40%;
-    height: 5%;
-    background-color: #D8E7FD;
-    color: black;
-    border-radius: 50px;
-    margin: auto;
-    display: flex;
-    margin-top: 3rem;
-    flex-direction: row;
+  width: 40%;
+  height: 5%;
+  background-color: #d8e7fd;
+  color: black;
+  border-radius: 50px;
+  margin: auto;
+  display: flex;
+  margin-top: 3rem;
+  flex-direction: row;
 
-    &:hover {
-        transform: translateY(-5px);
-        box-shadow: 4px 4px 25px #3E86F5;
-        cursor: pointer;
-    }
+  &:hover {
+    transform: translateY(-5px);
+    box-shadow: 4px 4px 25px #3e86f5;
+    cursor: pointer;
+  }
 }
 
 .actual-info-container {
-    display: flex;
-    align-items: center;
-    justify-content: space-around;
-    background-color: #002148;
-    height: 50px;
-    position: sticky;
-    top: 0;
-    z-index: 10;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  background-color: #002148;
+  height: 50px;
+  position: sticky;
+  top: 0;
+  z-index: 10;
 
-    & p {
-        display: flex;
-        justify-content: center;
-        background-color: #3E86F5;
-        width: 9rem;
-        border-radius: 4px;
-    }
+  & p {
+    display: flex;
+    justify-content: center;
+    background-color: #3e86f5;
+    width: 9rem;
+    border-radius: 4px;
+  }
 }
 
 .error-msg {
-
-    color: black;
+  color: black;
 }
 
 .airline-logo {
-    width: 30%;
-    border-radius: 50px;
-    margin: 15px;
+  width: 30%;
+  border-radius: 50px;
+  margin: 15px;
 }
 
 .airline-logo img {
-    width: 100%;
-    height: 100%;
-    border-radius: 50px;
+  width: 100%;
+  height: 100%;
+  border-radius: 50px;
 }
 
 .ariline-title {
-    color: #002148;
+  color: #002148;
 }
 
 .info {
-    display: flex;
-    width: 70%;
-    flex-direction: column;
+  display: flex;
+  width: 70%;
+  flex-direction: column;
 }
 
 .flight-segments {
-    display: flex;
-    align-items: center;
-    height: 100%;
+  display: flex;
+  align-items: center;
+  height: 100%;
 }
 
 .departures-arrivals {
-    width: 100%;
-    height: 50%;
-    margin: 4px;
+  width: 100%;
+  height: 50%;
+  margin: 4px;
+  display: flex;
+
+  & .hour {
+    margin: 23px;
     display: flex;
+    align-items: center;
+  }
 
-    & .hour {
-        margin: 23px;
-        display: flex;
-        align-items: center;
-    }
+  & img {
+    width: 10%;
+  }
 
-    & img {
-        width: 10%;
-    }
-
-    & .airport-name {
-        margin: 23px;
-        display: flex;
-        align-items: center;
-    }
+  & .airport-name {
+    margin: 23px;
+    display: flex;
+    align-items: center;
+  }
 }
 
 .non-stop {
-    color: red;
+  color: red;
 }
 </style>
--- src/components/SearchFlight/SearchFlight.vue
+++ src/components/SearchFlight/SearchFlight.vue
@@ -1,300 +1,325 @@
 <template>
-    <div class="nav">
-        <h3 class="nav-tittle">Millones de Vuelos al mejor precio</h3>
-        <div class="nav-form">
-            <div class="nav-options">
-                <div class="option-wrapper">
-                    <label class="nav-info">Desde {{ departureIata }}</label>
-                    <input v-debounce:500ms="getSuggestion" class="nav-description" placeholder="País, ciudad o aeropuerto"
-                        @input="updateFlight" @focus="toggleDropdownLocation" :value="departureIata">
-
-                    <DropDownLocation :isVisible="showDropDownLocation" :suggestion=suggestionDeparture
-                        :setIsVisible=setDropDownLocationVisibility />
-                </div>
-                <div class="option-wrapper">
-                    <label class="nav-info">A {{ arrivalIata }}</label>
-                    <input v-debounce:500ms="getSuggestionArrival" class="nav-description"
-                        placeholder="País, ciudad o aeropuerto" @input="uptdateArrivalIata" @focus="toggleDropdownArrival"
-                        :value="arrivalIata">
-                    <DropDownArrival :isVisible="showDropDownArrival" :suggestion=suggestionArrival
-                        :setIsVisible=setDropDownArrivalVisibility />
-                </div>
-
-
-                <div class=" option-wrapper">
-                    <label class="nav-info">Ida {{ trialDate ? formatDate(trialDate) : '' }}</label>
-                    <VueDatePicker v-model="trialDate" :format="formatDate"></VueDatePicker>
-                    <!-- <input class="nav-description" type="date" @input="updateDepartureDate"> -->
-                </div>
-                <div class="option-wrapper">
+  <div class="nav">
+    <h3 class="nav-tittle">Millones de Vuelos al mejor precio</h3>
+    <div class="nav-form">
+      <div class="nav-options">
+        <div class="option-wrapper">
+          <label class="nav-info">Desde {{ departureIata }}</label>
+          <input
+            v-debounce:500ms="getSuggestion"
+            class="nav-description"
+            placeholder="País, ciudad o aeropuerto"
+            @input="updateFlight"
+            @focus="toggleDropdownLocation"
+            :value="departureIata"
+          />
+          <DropDownLocation
+            :isVisible="showDropDownLocation"
+            :suggestion="suggestionDeparture"
+            :setIsVisible="setDropDownLocationVisibility"
+          />
+        </div>
+        <div class="option-wrapper">
+          <label class="nav-info">A {{ arrivalIata }}</label>
+          <input
+            v-debounce:500ms="getSuggestionArrival"
+            class="nav-description"
+            placeholder="País, ciudad o aeropuerto"
+            @input="uptdateArrivalIata"
+            @focus="toggleDropdownArrival"
+            :value="arrivalIata"
+          />
+          <DropDownArrival
+            :isVisible="showDropDownArrival"
+            :suggestion="suggestionArrival"
+            :setIsVisible="setDropDownArrivalVisibility"
+          />
+        </div>
+        <div class="option-wrapper">
+          <label class="nav-info"
+            >Ida {{ prevDate ? formatDate(prevDate) : "" }}</label
+          >
+          <VueDatePicker
+            v-model="prevDate"
+            :format="formatDate"
+          ></VueDatePicker>
+        </div>
+        <!-- 
+                    Posible feed
+                    <div class="option-wrapper">
                     <label class="nav-info">Vuelta</label>
                     <input class="nav-description" type="date">
-                </div>
-                <div class="option-wrapper">
-
-                    <label class="nav-info" @click="toggleDropdown">Viajeros</label>
-                    <span class="nav-description" @click="toggleDropdown">
-                        {{ adult }} adultos
-                    </span>
-                    <DropDown :isVisible=isDropdownOpen :adultCount=adult :incrementAdults=incrementAdultCount
-                        :decrementAdults=decrementAdults :onReadyClick=handleReadyClick />
-                </div>
-            </div>
-            <router-link class="search" to="/flights" @click="fetchFlightOffers">Buscar</router-link>
-
-
+                </div> -->
+        <div class="option-wrapper">
+          <label class="nav-info" @click="toggleDropdown">Viajeros</label>
+          <span class="nav-description" @click="toggleDropdown">
+            {{ adult }} adultos
+          </span>
+          <DropDown
+            :isVisible="isDropdownOpen"
+            :adultCount="adult"
+            :incrementAdults="incrementAdultCount"
+            :decrementAdults="decrementAdults"
+            :onReadyClick="handleReadyClick"
+          />
         </div>
+      </div>
+      <router-link class="search" to="/flights" @click="fetchFlightOffers"
+        >Buscar</router-link
+      >
     </div>
+  </div>
 </template>
 
 <script>
-import { suggestLocation } from "../../stores/modules/getFlights.js"
-import { flightSearchStore } from "../../stores/counter.js"
+import { suggestLocation } from "../../stores/modules/getFlights.js";
+import { flightSearchStore } from "../../stores/counter.js";
 import { mapActions, mapState } from "pinia";
-import DropDown from "./components/DropDown/DropDown.vue"
+import DropDown from "./components/DropDown/DropDown.vue";
 import DropDownLocation from "./components/DropDown/DropDownLocation.vue";
-import { vue3Debounce } from 'vue-debounce'
-import DropDownArrival from "./components/DropDown/DropDownArrival.vue"
-import VueDatePicker from '@vuepic/vue-datepicker';
-import '@vuepic/vue-datepicker/dist/main.css'
-import { getFlights } from '../../stores/modules/getFlights.js'
+import { vue3Debounce } from "vue-debounce";
+import DropDownArrival from "./components/DropDown/DropDownArrival.vue";
+import VueDatePicker from "@vuepic/vue-datepicker";
+import "@vuepic/vue-datepicker/dist/main.css";
+import { getFlights } from "../../stores/modules/getFlights.js";
 
 export default {
-    components: {
-        DropDown,
-        DropDownLocation,
-        DropDownArrival,
-        VueDatePicker
+  components: {
+    DropDown,
+    DropDownLocation,
+    DropDownArrival,
+    VueDatePicker,
+  },
+  data() {
+    return {
+      isDropdownOpen: false,
+      isDropdownOpenLocation: false,
+      isDropdownOpenArrival: false,
+      dataFromDropDown: "",
+      suggestionDeparture: {},
+      suggestionArrival: {},
+      prevDate: "",
+    };
+  },
+  directives: {
+    debounce: vue3Debounce({}),
+  },
+  computed: {
+    ...mapState(flightSearchStore, [
+      "departureIata",
+      "arrivalIata",
+      "departureDate",
+      "adult",
+    ]),
+    showDropDownLocation() {
+      return this.isDropdownOpenLocation && !!this.departureIata.length;
     },
-    data() {
-        return {
-
-            isDropdownOpen: false,
-            isDropdownOpenLocation: false,
-            isDropdownOpenArrival: false,
-            dataFromDropDown: "",
-            FlightsOffers: "",
-            AirlineLogo: [],
-            suggestionDeparture: {},
-            suggestionArrival: {},
-            trialDate: ""
-        };
+    showDropDownArrival() {
+      return this.isDropdownOpenArrival && !!this.arrivalIata.length;
     },
-    directives: {
-        debounce: vue3Debounce({})
+  },
+  methods: {
+    updateFlight(e) {
+      this.setdepartureIata(e.target.value);
     },
-    computed: {
-        ...mapState(flightSearchStore, [
-            'departureIata',
-            'arrivalIata',
-            'departureDate',
-            'adult',
-        ]),
-        showDropDownLocation() {
-            return this.isDropdownOpenLocation && !!this.departureIata.length
-        },
-        showDropDownArrival() {
-            return this.isDropdownOpenArrival && !!this.arrivalIata.length
-        }
+    uptdateArrivalIata(e) {
+      this.setarrivalIata(e.target.value);
     },
-    methods: {
-        updateFlight(e) {
-            this.setdepartureIata(e.target.value)
-        },
-        uptdateArrivalIata(e) {
-            this.setarrivalIata(e.target.value);
-        },
-        // updateDepartureDate(e) {
-        //     this.setdepartureDate(e);
-        // },
-
-        // DropDown functions
-        toggleDropdown() {
-            this.isDropdownOpen = !this.isDropdownOpen;
-        },
-        receiveDataFromDropDown(data) {
-            this.dataFromDropDown = data;
-        },
-        handleReadyClick() {
-            this.isDropdownOpen = false;
-        },
-        // end of DropDown functions
-        async getSuggestion() {
-            this.suggestionDeparture = this.departureIata.length && await suggestLocation(this.departureIata)
-            console.log('sugestionDepart', this.suggestionDeparture)
-        },
-        async getSuggestionArrival() {
-            this.suggestionArrival = this.departureIata.length && await suggestLocation(this.arrivalIata)
-        },
-        toggleDropdownLocation() {
-            this.isDropdownOpenLocation = !this.isDropdownOpenLocation;
-        },
-        setDropDownLocationVisibility(newVisibility) {
-            this.isDropdownOpenLocation = newVisibility
-        },
-        toggleDropdownArrival() {
-            this.isDropdownOpenArrival = !this.isDropdownOpenArrival;
-        },
-        setDropDownArrivalVisibility(newVisibility) {
-            this.isDropdownOpenArrival = newVisibility
-        },
-        formatDate(date) {
-            const day = date.getDate();
-            const month = date.getMonth() + 1;
-            const year = date.getFullYear();
-            const formatedDate = `${year}-${month}-${day}`;
-            this.setdepartureDate(formatedDate)
-            return formatedDate;
-        },
-        async fetchFlightOffers() {
-            const data = await getFlights(this.departureIata, this.arrivalIata, this.departureDate, this.adult)
-            this.setFlightOffers(data)
-        },
-        ...mapActions(flightSearchStore, ['setdepartureIata', 'setarrivalIata', 'setdepartureDate', 'incrementAdultCount', 'decrementAdults', 'setFlightOffers']),
-    }
-}
-
+    toggleDropdown() {
+      this.isDropdownOpen = !this.isDropdownOpen;
+    },
+    receiveDataFromDropDown(data) {
+      this.dataFromDropDown = data;
+    },
+    handleReadyClick() {
+      this.isDropdownOpen = false;
+    },
+    // end of DropDown functions
+    async getSuggestion() {
+      this.suggestionDeparture =
+        this.departureIata.length &&
+        (await suggestLocation(this.departureIata));
+      console.log("sugestionDepart", this.suggestionDeparture);
+    },
+    async getSuggestionArrival() {
+      this.suggestionArrival =
+        this.departureIata.length && (await suggestLocation(this.arrivalIata));
+    },
+    toggleDropdownLocation() {
+      this.isDropdownOpenLocation = !this.isDropdownOpenLocation;
+    },
+    setDropDownLocationVisibility(newVisibility) {
+      this.isDropdownOpenLocation = newVisibility;
+    },
+    toggleDropdownArrival() {
+      this.isDropdownOpenArrival = !this.isDropdownOpenArrival;
+    },
+    setDropDownArrivalVisibility(newVisibility) {
+      this.isDropdownOpenArrival = newVisibility;
+    },
+    formatDate(date) {
+      const day = date.getDate();
+      const month = date.getMonth() + 1;
+      const year = date.getFullYear();
+      const formatedDate = `${year}-${month}-${day}`;
+      this.setdepartureDate(formatedDate);
+      return formatedDate;
+    },
+    async fetchFlightOffers() {
+      const data = await getFlights(
+        this.departureIata,
+        this.arrivalIata,
+        this.departureDate,
+        this.adult
+      );
+      this.setFlightOffers(data);
+    },
+    ...mapActions(flightSearchStore, [
+      "setdepartureIata",
+      "setarrivalIata",
+      "setdepartureDate",
+      "incrementAdultCount",
+      "decrementAdults",
+      "setFlightOffers",
+    ]),
+  },
+};
 </script>
 
 <style>
 :root {
-    --dp-border-radius: 0px;
-    --dp-background-color: #d9e2f2;
+  --dp-border-radius: 0px;
+  --dp-background-color: #d9e2f2;
 }
 
-.dp__theme_light {}
+.dp__theme_light {
+}
 
 .nav {
-    background-color: #1b55b0;
-    width: 100%;
-    padding: 1rem 2rem;
+  background-color: #1b55b0;
+  width: 100%;
+  padding: 1rem 2rem;
 }
 
 .nav-tittle {
-    color: #FFFFFF;
-    text-shadow: 0px 4px 4px #00000040;
-    font-size: 40px;
-    font-style: normal;
-    font-weight: 400;
-    line-height: normal;
-    padding-bottom: 2rem;
+  color: #ffffff;
+  text-shadow: 0px 4px 4px #00000040;
+  font-size: 40px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  padding-bottom: 2rem;
 }
 
 .buttons-container {
-    display: flex;
-    gap: 0.5rem;
-
+  display: flex;
+  gap: 0.5rem;
 }
 
 .nav-button {
-    border-radius: 10px;
-    border: none;
-    background: #00214880;
-    color: #FFFFFF;
-    cursor: pointer;
-    font-size: 1rem;
-    padding: 0.25rem 1rem;
+  border-radius: 10px;
+  border: none;
+  background: #00214880;
+  color: #ffffff;
+  cursor: pointer;
+  font-size: 1rem;
+  padding: 0.25rem 1rem;
 }
 
 .nav-form {
-    display: flex;
-    gap: 1rem;
-    padding-top: 0.5rem;
+  display: flex;
+  gap: 1rem;
+  padding-top: 0.5rem;
 }
 
 .nav-options {
-    display: flex;
-    gap: 0.25rem;
+  display: flex;
+  gap: 0.25rem;
 }
 
-
 .option-wrapper {
-    background-color: #ffffffd4;
-    display: flex;
-    flex-direction: column;
-    padding: 0.5rem 1rem;
-    gap: 0.25rem;
-    position: relative;
-    min-width: 8rem;
+  background-color: #ffffffd4;
+  display: flex;
+  flex-direction: column;
+  padding: 0.5rem 1rem;
+  gap: 0.25rem;
+  position: relative;
+  min-width: 8rem;
 }
 
 .option-wrapper:first-child {
-    border-radius: 0.5rem 0 0 0.5rem;
+  border-radius: 0.5rem 0 0 0.5rem;
 }
 
 .option-wrapper:last-child {
-    border-radius: 0 0.5rem 0.5rem 0;
+  border-radius: 0 0.5rem 0.5rem 0;
 }
 
 .search {
-    border-radius: 10px;
-    background: rgb(0, 32, 72);
-    color: #FFFFFF;
-    border: none;
-    font-size: 26px;
-    padding: 0 1rem;
-    display: flex;
-    align-items: center;
-    text-decoration: none;
-    transition: all 0.1s ease-in;
+  border-radius: 10px;
+  background: rgb(0, 32, 72);
+  color: #ffffff;
+  border: none;
+  font-size: 26px;
+  padding: 0 1rem;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  transition: all 0.1s ease-in;
 
-    &:hover {
-        background: #FFFFFF;
-        color: rgb(0, 32, 72);
-    }
+  &:hover {
+    background: #ffffff;
+    color: rgb(0, 32, 72);
+  }
 }
 
-
-
 .nav-info {
-    color: #393838;
-    text-shadow: 0px 4px 4px #00000040;
+  color: #393838;
+  text-shadow: 0px 4px 4px #00000040;
 }
 
 .nav-description {
-    padding: 0.5rem;
-    background-color: #FFFFFF;
-    border: 1px solid #ddd;
-    color: #393838;
-    font-size: 1rem;
-    line-height: 1rem;
-    transition: all 0.1s ease-in;
+  padding: 0.5rem;
+  background-color: #ffffff;
+  border: 1px solid #ddd;
+  color: #393838;
+  font-size: 1rem;
+  line-height: 1rem;
+  transition: all 0.1s ease-in;
 
-    &:hover {
-        border: 1px solid #aaaeb7;
-    }
+  &:hover {
+    border: 1px solid #aaaeb7;
+  }
 
-    &:focus {
-        outline: none;
-    }
+  &:focus {
+    outline: none;
+  }
 }
 
 .nav-description:hov .search {
-    border-radius: 10px;
-    background: rgba(0, 33, 72, 0.50);
-    color: #FFFFFF;
-    border: none;
-    font-size: 26px;
-    padding: 0 1rem;
+  border-radius: 10px;
+  background: rgba(0, 33, 72, 0.5);
+  color: #ffffff;
+  border: none;
+  font-size: 26px;
+  padding: 0 1rem;
 }
 
-
-
 @media (max-width: 1028px) {
-    .nav-options {
-        flex-direction: column;
-    }
+  .nav-options {
+    flex-direction: column;
+  }
 
-    .nav-form {
-        flex-direction: column;
-    }
+  .nav-form {
+    flex-direction: column;
+  }
 
-    .option-wrapper:first-child {
-        border-radius: 0.5rem 0.5rem 0 0;
-    }
+  .option-wrapper:first-child {
+    border-radius: 0.5rem 0.5rem 0 0;
+  }
 
-    .option-wrapper:last-child {
-        border-radius: 0 0 0.5rem 0.5rem;
-    }
+  .option-wrapper:last-child {
+    border-radius: 0 0 0.5rem 0.5rem;
+  }
 }
 </style>
