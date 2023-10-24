3ea477b6b780485f18c9e8cacb0edfb1e622e392
Elena Expósito <elexla97@gmail.com>
2023-10-24 22:08:24 +0200

    Add TipsResume.vue with a card and waves-move

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -5,12 +5,15 @@
     </div>
     <FamousPlaces />
     <ProgressBar />
+    <TipsResume />
 </template>
+
 <script>
 import FamousPlaces from '../components/FamousPlaces.vue';
 import NavigationTopBar from '../components/NavigationTopBar.vue';
 import SearchFlight from '../components/SearchFlight/SearchFlight.vue';
 import ProgressBar from '../components/ProgressBar.vue';
+import TipsResume from '../components/TipsResume.vue';
 
 export default {
     name: 'HomePage',
@@ -18,8 +21,10 @@ export default {
         NavigationTopBar,
         SearchFlight,
         FamousPlaces,
-        ProgressBar
+        ProgressBar,
+        TipsResume
     }
 }
 </script>
+
 <style scoped></style>
