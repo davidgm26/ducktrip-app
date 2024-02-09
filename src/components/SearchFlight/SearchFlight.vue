4bffc1d40bbb3a2cd13670c0a610066d5e42434a
Carlos Concepcion <concep131@gmail.com>
2024-02-09 10:38:30 +0000

    formatDate has been changed using padStart instead of getDate.
    Now const day and month can write 2 digits if its needed.

--- src/components/SearchFlight/SearchFlight.vue
+++ src/components/SearchFlight/SearchFlight.vue
@@ -171,8 +171,8 @@ export default {
       this.isDropdownOpenArrival = newVisibility;
     },
     formatDate(date) {
-      const day = date.getDate();
-      const month = date.getMonth() + 1;
+      const day = String(date.getDate()).padStart(2, '0');
+      const month = String(date.getMonth() + 1).padStart(2, '0');
       const year = date.getFullYear();
       const formatedDate = `${year}-${month}-${day}`;
       this.setdepartureDate(formatedDate);
